6abbcc0192c4fbaf785e5b4a21a22665d6146dc2
Megan Howley <meglhowley@gmail.com>
2021-07-08 19:11:49 -0400

    designed home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css'
+import './Meg.css'
 import { Switch, Route } from 'react-router-dom'
 import React, { useEffect, useState } from 'react'
 import Client from './services'
@@ -10,6 +11,8 @@ function App() {
   const [selectedStory, setSelectedStory] = useState(null)
   const [songs, setSongs] = useState([])
   const [selectedSong, setSelectedSong] = useState(null)
+  const [expandSongs, toggleExpandSongs] = useState(false)
+  const [expandStories, toggleExpandStories] = useState(false)
 
   const getStories = async () => {
     let res = await Client.get('/stories')
@@ -23,16 +26,39 @@ function App() {
     setSongs(res.data)
   }
 
+  const handleClickedStories = () => {
+    toggleExpandStories(true)
+    console.log(expandStories)
+  }
+
+  const handleClickedSongs = () => {
+    toggleExpandSongs(true)
+    console.log(expandSongs)
+  }
+
   useEffect(() => {
     getStories()
     getSongs()
   }, [])
 
   return (
-    <div className="App">
-      Homepage
-      <AllStories stories={stories} />
-      <AllSongs songs={songs} />
+    <div className="main-wrapper">
+      <div className="left">
+        <div onClick={handleClickedStories} className="story-btn">
+          STORIES
+        </div>
+        <img
+          className="fiya"
+          src="https://media1.giphy.com/media/UqUHuT6y9mK5HfsYFm/giphy.gif?cid=6c09b952a8nhlnlmupyayo2031aorn6k5zva5ee5ay5d6goj&rid=giphy.gif&ct=s"
+        />
+      </div>
+      <div className="right">
+        <div onClick={handleClickedSongs} className="songs-btn">
+          SONGS
+        </div>
+      </div>
+      {/* <AllStories stories={stories} />
+      <AllSongs songs={songs} /> */}
     </div>
   )
 }
