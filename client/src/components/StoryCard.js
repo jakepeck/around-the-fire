fd400aff69160e25455eb0a6ad9a7e928f0a35a9
Megan Howley <meglhowley@gmail.com>
2021-07-09 14:13:53 -0400

    post comment works

--- client/src/components/StoryCard.js
+++ client/src/components/StoryCard.js
@@ -5,12 +5,33 @@ const StoryCard = (props) => {
   const [content, setContent] = useState(false)
   const [comments, setComments] = useState([])
   const [showComments, toggleShowComments] = useState(false)
+  const [commentForm, setCommentForm] = useState({
+    content: '',
+    story_id: null
+  })
 
   const handleClick = async (id) => {
     setContent(!content)
     const res = await Client.get(`/stories/${id}`)
     const comments = res.data.comments
+    setCommentForm({ ...commentForm, story_id: id })
     setComments(comments)
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setCommentForm({ ...commentForm, [name]: value })
+    console.log(commentForm)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const newComment = await Client.post(`/comments`, commentForm)
+    setCommentForm({
+      ...commentForm,
+      content: ''
+    })
+    setComments([...comments, newComment.data])
   }
 
   return (
@@ -26,7 +47,13 @@ const StoryCard = (props) => {
           </button>
           {showComments ? (
             <div>
-              <button>+ Add Comment</button>
+              <textarea
+                name="content"
+                placeholder="Add comment here"
+                value={commentForm.content}
+                onChange={handleChange}
+              ></textarea>
+              <button onClick={handleSubmit}>+ Add Comment</button>
               <br />
               {comments.map((comment, index) => (
                 <div className="comment-card">Comment: {comment.content}</div>
